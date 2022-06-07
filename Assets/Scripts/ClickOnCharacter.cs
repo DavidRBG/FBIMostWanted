e7040969dfe74b983166c08866b66796762fc909
DavidRBG <davidbolivarg@gmail.com>
2022-06-07 22:01:01 +0100

    Los Popup Funcionan

--- Assets/Scripts/ClickOnCharacter.cs
+++ Assets/Scripts/ClickOnCharacter.cs
@@ -11,13 +11,17 @@ public class ClickOnCharacter : MonoBehaviour
     public AudioClip wrong;
     public AudioClip right;
     public GameObject WinPopUp;
+    public GameObject FailPopUp;
 
+    public float fail;
     public float timer;
-    public float fail =0f;
-    public float tiempoTranscurrido;
+    public float timerCloseFail;
+
+    public float tiempoTranscurrido=0f;
     //public float addingAllThePoints;
 
     public generador_position_character posicionesrellenar;
+
 
 
     void Update()
@@ -33,14 +37,22 @@ public class ClickOnCharacter : MonoBehaviour
         {
             sonidos = gameObject.GetComponent<AudioSource>() ;
             sonidos.PlayOneShot(right);
-            WinPopUp.SetActive(true);
+            GameObject popUp = Instantiate(WinPopUp);
+            popUp.SetActive(true);
+
         }
 
         if (!IsWally)
         {
+            timerCloseFail += Time.deltaTime;
             sonidos = gameObject.GetComponent<AudioSource>();
             sonidos.PlayOneShot(wrong);
             fail += 10f;
+            GameObject popUp = Instantiate(FailPopUp);
+            popUp.SetActive(true);
+
+
+
         }
     }
 
